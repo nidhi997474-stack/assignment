2d05e6440db8a1a759207ec976be64ad84734e97
nidhi997474-stack <nidhi997474@gmail.com>
2026-08-27 13:20:42 +0530

    session 17 modules and packages

---
+++ follower_count.py
@@ -0,0 +1,5 @@
+from insta_utils import format_follower_count
+
+print(format_follower_count(1500))
+print(format_follower_count(2300000))
+print(format_follower_count(500))
---
+++ insta_utils.py
@@ -0,0 +1,7 @@
+def format_follower_count(n):
+    if n >= 1000000:
+        return str(round(n / 1000000, 1)) + "M"
+    elif n >= 1000:
+        return str(round(n / 1000, 1)) + "K"
+    else:
+        return str(n)
